f88ad21292f46f37f34f73b3d0df4b405b8214f2
Ariana Cabana <acabana.0904@gmail.com>
2017-04-19 12:39:29 -0500

    insertando elementos al html con js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,34 @@
-//variables principales
-var divContenedor= document.getElementById("gallery");
-var docFragment = document.createDocumentFragment();
+var body = document.getElementsByTagName("body")[0];
+
+// crear elementos para HTML
+var tittle = document.createElement("h1");
+tittle.innerText = "Nuestras Coders";
+var hr = document.createElement("hr");
+
+//insertando elementos al body
+body.appendChild(tittle);
+body.appendChild(hr);
+
+//nombre de las coders
 var infoCoders = ["Fiorella Quispe","Lourdes Vilchez","Rossmery Maldonado",
 "Yessenia Huamán","Grecia Rayme","Janine Vega"];
+
+// un for para mostrar a todas las coders
+window.onload = function() {
+  for (var i = 1; i <= infoCoders.length; i++) {
+    var div = document.createElement("div");
+    var img = document.createElement("img");
+    var span = document.createElement("span");
+    var nombreCoder = document.createTextNode(infoCoders[i-1]);
+
+    img.setAttribute("src","assets/img/"+i+".png");
+    img.setAttribute("alt",i+".png");
+    img.setAttribute("class", "coder");
+    div.setAttribute("class", "foto");
+    span.setAttribute("class", "nombre");
+
+    div.appendChild(img);
+    div.appendChild(span);
+    body.appendChild(div);
+  }
+}
