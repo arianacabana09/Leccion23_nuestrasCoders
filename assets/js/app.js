e94777054c0446ebc3be42ae82c3f44b6d1754b4
Ariana Cabana <acabana.0904@gmail.com>
2017-04-19 12:12:05 -0500

    maquetacion inicial

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+//variables principales
+var divContenedor= document.getElementById("gallery");
+var docFragment = document.createDocumentFragment();
+var infoCoders = ["Fiorella Quispe","Lourdes Vilchez","Rossmery Maldonado",
+"Yessenia Huamán","Grecia Rayme","Janine Vega"];
